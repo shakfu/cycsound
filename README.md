b7f28ea6c9d4d92e03c9556ab77f5730eacb15e6
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2024-06-18 23:11:08 +0300

    windows fix

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Towards a cython wrapper of the csound api `csound.h`
 ## Building
 
 Requires `cython` and csound to be installed on your system
+
+To build on macOS:
+
+```sh
+python3 setup.py build
+```
+
+on Windows:
+
+```sh
+python setup.py build
+```
--- setup.py
+++ setup.py
@@ -1,36 +1,51 @@
 import os
-# from distutils.core import setup
-# from distutils.extension import Extension
-from setuptools import setup, Extension
+import platform
+from pathlib import Path
 
+from setuptools import setup, Extension
 
 from Cython.Build import cythonize
 
-os.environ['LDFLAGS'] = " ".join([
+PLATFORM = platform.system()
+INCLUDE_DIRS = []
+LIBRARIES = []
+LIBRARY_DIRS = []
+
+if PLATFORM == "Darwin":
+    os.environ['LDFLAGS'] = " ".join([
         "-framework CsoundLib64",
         "-framework CoreFoundation",
         "-framework AudioUnit",
         "-framework AudioToolbox",
         "-framework CoreAudio",
         "-F /Library/Frameworks",
-])
+    ])
+    INCLUDE_DIRS.append("/Library/Frameworks/CsoundLib64.framework/Headers")
+elif PLATFORM == "Windows":
+    base = Path("C:\\Program Files\\Csound6_x64")
+    bin = base / 'bin'
+    include = base / 'include' / 'csound'
+    libdir = base / 'lib'
+    INCLUDE_DIRS.append(str(include))
+    LIBRARY_DIRS.append(str(libdir))
+    LIBRARIES.extend([
+        'csound64',
+        'csnd6',
+    ])
+
+elif PLATFORM == "Linux":
+    pass
+else:
+    pass
 
 extensions = [
     Extension("csnd", ["csnd.pyx"],
         # define_macros = [
         #     ('PD', 1),
         # ],
-        include_dirs=[
-            "/Library/Frameworks/CsoundLib64.framework/Headers",
-        ],
-        libraries = [
-            'm',
-            'dl',
-            'pthread',
-        ],
-        library_dirs=[],
-        extra_objects=[
-        ],
+        include_dirs=INCLUDE_DIRS,
+        libraries = LIBRARIES,
+        library_dirs=LIBRARY_DIRS,
     ),
 ]
 
